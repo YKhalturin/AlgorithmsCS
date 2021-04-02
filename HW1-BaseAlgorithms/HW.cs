39b7e00b64848c4c1f7c8ba30855263203c3f390
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2021-04-02 11:35:06 +0300

    Создал решение по залаче  Найти максимальное из четырех чисел. Массивы не использовать.

--- HW1-BaseAlgorithms/HW.cs
+++ HW1-BaseAlgorithms/HW.cs
@@ -2,17 +2,19 @@
 
 namespace HW1_BaseAlgorithms
 {
+    /*
+     * Халтурин Юрий
+     */
+
     partial class HW
     {
         /*
          * 1. Ввести вес и рост человека. Рассчитать и вывести индекс массы тела по формуле I=m/(h*h);
          * где m-масса тела в килограммах, h - рост в метрах.
-         *
-         * Халтурин Юрий
          */
+
         public static void Task1()
         {
-            Console.WriteLine("Добро пожаловать в прогрумму Анкета");
             Console.Write("Введите вашу массу тела в килограммах: ");
             string weightStr = Console.ReadLine();
             int weight = Convert.ToInt32(weightStr);
@@ -27,21 +29,33 @@ namespace HW1_BaseAlgorithms
         }
 
         /*
-         *
-         *
-         *
+         * 2. Найти максимальное из четырех чисел. Массивы не использовать.
          */
+
+
+        public static int Min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+        public static int GetMin4Numbers(int a, int b, int c, int d)
+        {
+            return Min(Min(a,b), Min(c, d));
+        }
 
         public static void Task2()
         {
+            int a = 23;
+            int b = 22;
+            int c = 54;
+            int d = -5;
 
-
+            Console.WriteLine($"Минимальное из 4х чисел {a}, {b}, {c}, {d} : {GetMin4Numbers(a, b, c, d)}");
         }
 
         /*
-         *
-         *
-         *
+         * 3. Написать программу обмена значениями двух целочисленных переменных:
+         * a. с использованием третьей переменной;
+         * b. *без использования третьей переменной.
          */
 
         public static void Task3()
@@ -51,10 +65,9 @@ namespace HW1_BaseAlgorithms
         }
 
         /*
-         *
-         *
-         *
+         * 4. Написать программу нахождения корней заданного квадратного уравнения.
          */
+
         public static void Task4()
         {
 
