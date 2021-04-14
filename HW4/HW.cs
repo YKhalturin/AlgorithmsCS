790a851ba7875857c57cc38aa80128e07c14f496
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2021-04-14 18:44:20 +0300

    Реализовал задачу о нахождении длины максимальной последовательности с помощью матрицы.

--- HW4/HW.cs
+++ HW4/HW.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HW4
 {
@@ -20,6 +21,7 @@ namespace HW4
                     Console.Write("{0,5}", a[i, j]);
                 Console.WriteLine();
             }
+
             Console.WriteLine();
         }
 
@@ -41,6 +43,7 @@ namespace HW4
                 }
             }
 
+            // добавляем запретные клетки
             map[1, 1] = 0;
             map[2, 2] = 0;
             map[3, 3] = 0;
@@ -56,12 +59,14 @@ namespace HW4
                     {
                         a[i, j] = 0;
                     }
+
                     if (map[i, j] == 1)
                     {
                         a[i, j] = a[i - 1, j] + a[i, j - 1];
                     }
                 }
             }
+
             PrintArray(a, "Количество маршрутов с препятствиями:");
         }
 
@@ -69,10 +74,79 @@ namespace HW4
         /*
          * 2. Решить задачу о нахождении длины максимальной последовательности с помощью матрицы.
          */
-        public static void Task2()
-        {
+        public static int n, m;
+        public static int[,] c = new int[20, 20];
+        public static char[] x = new char [20];
+        public static char[] y = new char[20];
+        public static char[,] b = new char[20, 20];
 
+        public static void PrintSequence(int i, int j)
+        {
+            if (i == 0 || j == 0)
+                return;
+            if (b[i, j] == 'c')
+            {
+                PrintSequence(i - 1, j - 1);
+                Console.Write(x[i - 1]);
+            }
+            else if (b[i, j] == 'u')
+                PrintSequence(i - 1, j);
+            else
+                PrintSequence(i, j - 1);
         }
 
+        public static void Lcs()
+        {
+            m = x.Length;
+            n = y.Length;
+
+            for (int i = 0; i < m; i++)
+            {
+                c[i, 0] = 0;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                c[0, i] = 0;
+            }
+            //c, u и l обозначают поперечное, восходящее и нисходящее направления соответственно(cross upper left)
+            //m - длина первой строки
+            //n - длина второй строки
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (x[i - 1] == y[j - 1])
+                    {
+                        c[i, j] = c[i - 1, j - 1] + 1;
+                        b[i, j] = 'c';
+                    }
+                    else if (c[i - 1, j] >= c[i, j - 1])
+                    {
+                        c[i, j] = c[i - 1, j];
+                        b[i, j] = 'u';
+                    }
+                    else
+                    {
+                        c[i, j] = c[i, j - 1];
+                        b[i, j] = 'l';
+                    }
+                }
+            }
+        }
+
+        public static void Task2()
+        {
+            Console.WriteLine("Введите первую последовательность:");
+            x = Console.ReadLine().ToCharArray();
+            Console.WriteLine("Введите вторую последовательность:");
+            y = Console.ReadLine().ToCharArray();
+            Console.WriteLine("Наибольшая общая последовательность:");
+            Lcs();
+            PrintSequence(m, n);
+            Console.WriteLine();
+
+        }
     }
 }
