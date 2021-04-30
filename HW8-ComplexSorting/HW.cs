c2e3370cdb7a3c4afb6d3bcc76106ef74d6ecaba
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2021-04-30 09:56:32 +0300

    Creted template

---
+++ HW8-ComplexSorting/HW.cs
@@ -0,0 +1,29 @@
+﻿namespace HW8_ComplexSorting
+{
+    partial class HW
+    {
+        /*
+         * 1. Реализовать сортировку подсчетом.
+         */
+        public static void Task1()
+        {
+           
+        }
+
+        /*
+         * 2. Реализовать быструю сортировку.
+         */
+        public static void Task2()
+        {
+
+        }
+
+        /*
+         * 3. *Реализовать сортировку слиянием.
+         */
+        public static void Task3()
+        {
+
+        }
+    }
+}
